0d8ccb8ed05d304903d2dc6a1bcd0b23771b527b
euna <dmsdk623@gmail.com>
2023-01-09 23:49:12 +0900

    타이틀 무브
    2023.01.09 23:48

--- Project_B/Assets/02_Scripts/MenuManager.cs
+++ Project_B/Assets/02_Scripts/MenuManager.cs
@@ -1,38 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class MenuManager : MonoBehaviour
 {
-    public GameObject menuSet;
-    public GameObject startSet;
-    public AudioSource musicsource;
-    void Update()
-    {
-        if (Input.GetButtonDown("Cancel"))
-        {
-            if (menuSet.activeSelf)
-            {
-                menuSet.SetActive(false);
-            }
-            else
-            {
-                menuSet.SetActive(true);
-            }
-        }
-    }
-    public void GameStart()
-    {
-        startSet.SetActive(false);
-    }
-    public void GameExit()
-    {
-        Application.Quit();
-        Debug.Log("종료하기");
-    }
+	public GameObject menuSet;
+	public GameObject startSet;
+	public AudioSource musicsource;
+	public float time = 0;
+	void Update()
+	{
+		time = Time.deltaTime;
+		if (Input.GetButtonDown("Cancel"))
+		{
+			if (menuSet.activeSelf)
+			{
+				menuSet.SetActive(false);
+			}
+			else
+			{
+				menuSet.SetActive(true);
+			}
+			GameStart();
+		}
+	}
+	public void GameStart()
+	{
+		startSet.GetComponent<RectTransform>().DOAnchorPosY(570f, 0.65f);
+	}
+	public void GameExit()
+	{
+		Application.Quit();
+		Debug.Log("종료하기");
+	}
 
-    public void SetMusicVolume(float volume)
-    {
-        musicsource.volume = volume;
-    }
+	public void SetMusicVolume(float volume)
+	{
+		musicsource.volume = volume;
+	}
 }
