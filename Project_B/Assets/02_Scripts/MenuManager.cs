bbafb870451b944cb53de9b1b9f7eaee84d1520e
euna <dmsdk623@gmail.com>
2023-01-09 19:04:16 +0900

    메뉴 구현
    2023.01.09 19:04

---
+++ Project_B/Assets/02_Scripts/MenuManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject menuSet;
+    public AudioSource musicsource;
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (menuSet.activeSelf)
+            {
+                menuSet.SetActive(false);
+            }
+            else
+            {
+                menuSet.SetActive(true);
+            }
+        }
+    }
+    public void GameExit()
+    {
+        Application.Quit();
+        Debug.Log("종료하기");
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        musicsource.volume = volume;
+    }
+}
