9df2411910663927c550c402ff2e507d6382368c
gyuhyeun <dean3264@naver.com>
2023-01-12 00:35:08 +0900

    Random

--- Project_B/Assets/02_Scripts/StageManager.cs
+++ Project_B/Assets/02_Scripts/StageManager.cs
@@ -29,9 +29,9 @@ public class StageManager : MonoBehaviour
     {
         Time.timeScale = 0f;
         panel.SetActive(true);
-        yield return new WaitForSecondsRealtime(2f);
+        yield return new WaitForSecondsRealtime(0.1f);
         print("break");
-        win.objNum++;
+        win.Gacha();
         win.Win();
         panel.SetActive(false);
         timer.TimerReset();
--- Project_B/Assets/02_Scripts/Wingyu.cs
+++ Project_B/Assets/02_Scripts/Wingyu.cs
@@ -6,11 +6,41 @@ public class Wingyu : MonoBehaviour
 {
     [SerializeField] public GameObject[] obj;
     [SerializeField] Transform objFactory;
-    public int objNum = 0;
-    
+    public List<int> GachaList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+    public List<int> ImshyList = new List<int>();
+    public int objNum;
+    public bool setting = true;
+
+    private void Start()
+    {
+        if (setting)
+        {
+            ImshyList = GachaList;
+            setting = false;
+        }
+    }
+    private void Update()
+    {
+        ResetList();
+    }
+    public void Gacha()
+    {
+        int rand = Random.Range(0, GachaList.Count);
+        print(GachaList[rand]);
+        objNum = rand;
+        GachaList.RemoveAt(rand);
+    }
     public void Win()
     {
         GameObject objs = Instantiate(obj[objNum]);
         objs.transform.position = objFactory.position;
+    }
+    public void ResetList()
+    {
+        if(GachaList.Count == 0)
+        {
+            GachaList = ImshyList;
+            setting = true;
+        }
     }
 }
