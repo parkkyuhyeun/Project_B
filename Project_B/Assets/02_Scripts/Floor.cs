739b89d19983946de52ade70a07005919f76c3d1
gyuhyeun <dean3264@naver.com>
2023-01-12 14:39:10 +0900

    뭐했더라

--- Project_B/Assets/02_Scripts/Floor.cs
+++ Project_B/Assets/02_Scripts/Floor.cs
@@ -7,13 +7,10 @@ using TMPro;
 
 public class Floor : MonoBehaviour
 {
-    [SerializeField] public GameObject leftStick;
-    [SerializeField] public GameObject rightStick;
     [SerializeField] public GameObject gameOverPanel;
     [SerializeField] LifeManager lifeM;
     [SerializeField] StageManager stageM;
     [SerializeField] TextMeshProUGUI stageText;
-    public bool isDead = false;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -23,20 +20,16 @@ public class Floor : MonoBehaviour
             lifeM.UpdateLifeIcon(lifeM.life);
             if (lifeM.life != 0)
             {
-                isDead = true;
                 FindObjectOfType<Timer>().TimerReset();
-                FindObjectOfType<Wingyu>().Win();
                 FindObjectOfType<Flipper>().ResetQuaternion();
-                isDead = false;
+                FindObjectOfType<Wingyu>().Win();
                 Destroy(collision.gameObject);
             }
             else
             {
-                isDead = true;
                 Time.timeScale = 0f;
                 stageText.text = $"{stageM.num + 1} Stage";
                 gameOverPanel.SetActive(true);
-                isDead = false;
             }
         }
     }
